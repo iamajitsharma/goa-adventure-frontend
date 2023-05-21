9d0ade89e939111e859bcaa2f72e4fc789920fda
silverag2002 <silverag2002@gmail.com>
2023-05-21 14:38:17 +0530

    Animation added to mobile view of sort by

--- frontend/src/components/common/FilterSearchBar.tsx
+++ frontend/src/components/common/FilterSearchBar.tsx
@@ -3,17 +3,25 @@ import Select from "react-dropdown-select";
 import { RxCross1 } from "react-icons/rx";
 import { AiOutlineSortAscending } from "react-icons/ai";
 import { BiFilterAlt } from "react-icons/bi";
-
+import { useState } from "react";
 export default function FilterSearchBar() {
+  const [showSortOption, setShowSortOption] = useState(false);
   const options = [
     { value: 0, label: "High Priority" },
     { value: 1, label: "High Relevance" },
     { value: 2, label: "High Ratings" },
   ];
   const onChange = () => {};
+  const handleShow = () => {
+    setShowSortOption(!showSortOption);
+  };
 
   return (
-    <div className=" flex flex-col lg:flex-row  lg:h-[60px] bg-white rounded-full lg:justify-between justify-center items-center px-6 mx-12 mt-6 drop-shadow-lg z-20">
+    <div
+      className={`flex flex-col lg:flex-row ${
+        !showSortOption ? "h-[60px] rounded-full" : "rounded-md h-[150px]"
+      } lg:h-[60px] bg-white lg:rounded-full lg:justify-between justify-center items-center px-6 mx-12 mt-6 drop-shadow-lg z-20`}
+    >
       <div className="lg:flex lg:flex-row hidden items-center ">
         <BsSearch />
         <input
@@ -35,7 +43,10 @@ export default function FilterSearchBar() {
       {/* Mobile View */}
       {/* <div> */}
       <div className="lg:hidden flex flex-row items-center w-full justify-center">
-        <div className="flex flex-row items-center justify-center w-1/2">
+        <div
+          className="flex flex-row items-center justify-center w-1/2"
+          onClick={() => handleShow()}
+        >
           <span className="px-2">Sort By</span>
           <AiOutlineSortAscending />
         </div>
@@ -45,15 +56,17 @@ export default function FilterSearchBar() {
         </div>
         {/* </div> */}
       </div>
-      <div className="flex flex-row ml-10 mr-3 items-center w-10/12 justify-center my-5">
-        <div className="z-50 mx-4 rounded-md bg-gray-200 w-3/5 ">
-          <Select
-            options={options}
-            onChange={(values) => onChange()}
-            className="w-full"
-          />
+      {showSortOption ? (
+        <div className="flex flex-row ml-10 mr-3 items-center w-10/12 justify-center my-5 transition-all duration-1000 ease-in-out">
+          <div className="z-50 mx-4 rounded-md bg-gray-200 w-3/5 ">
+            <Select
+              options={options}
+              onChange={(values) => onChange()}
+              className="w-full"
+            />
+          </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 }
