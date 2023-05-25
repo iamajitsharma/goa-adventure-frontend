bb2db523c20681f2457246fb4e6d4a23245497f9
Ajit Sharma <ajitsharma2021@gmail.com>
2023-05-25 21:38:22 +0530

    Privacy Policy Added

--- frontend/src/pages/privacy-policy.tsx
+++ frontend/src/pages/privacy-policy.tsx
@@ -1,16 +1,110 @@
 import Link from "next/link";
+import Container from "@/components/Container";
 import PrivacyPolicies from "@/components/disclosure/PrivacyPolicy";
+import { SlEarphonesAlt, SlLocationPin } from "react-icons/sl";
+import { AiOutlineMail } from "react-icons/ai";
+
+export const disclosure_navLink = [
+  {
+    id: "01",
+    path: "privacy-policy",
+    display: "Privacy Policy",
+  },
+  {
+    id: "02",
+    path: "terms-conditions",
+    display: "Terms & Conditions",
+  },
+  {
+    id: "03",
+    path: "cancellation-policy",
+    display: "Cancellation Policy",
+  },
+];
 
 const PrivacyPolicy = () => {
   return (
     <section className="font-poppins">
-      <div className="md:max-w-5xl bg-gray-300 mx-auto p-4">
-        <h3 className="text-center text-2xl font-semibold">Privacy Policy</h3>
-        <span className="text-base">Effective Date: 01-01-2021</span>
-        <div>
-          <PrivacyPolicies />
+      <Container>
+        <div className="flex md:flex-row h-full relative">
+          <div className="w-full lg:w-9/12 p-4">
+            <div className="">
+              <h2 className="text-2xl font-bold">Privacy Policy</h2>
+              <span className="text-base text-neutral-600">
+                Effective from 1st Jan 2021
+              </span>
+            </div>
+            <div className="disclosure">
+              <h4>Introduction</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                odit distinctio omnis totam? Dignissimos, consectetur similique
+                alias laborum distinctio voluptas sequi sapiente veritatis neque
+                accusantium iure illum, id autem amet? Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Iure odit distinctio omnis
+                totam? Dignissimos, consectetur similique alias laborum
+                distinctio voluptas sequi sapiente veritatis neque accusantium
+                iure illum, id autem amet? Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Iure odit distinctio omnis totam?
+                Dignissimos, consectetur similique alias laborum distinctio
+                voluptas sequi sapiente veritatis neque accusantium iure illum,
+                id autem amet? Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Iure odit distinctio omnis totam? Dignissimos,
+                consectetur similique alias laborum distinctio voluptas sequi
+                sapiente veritatis neque accusantium iure illum, id autem amet?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                odit distinctio omnis totam? Dignissimos, consectetur similique
+                alias laborum distinctio voluptas sequi sapiente veritatis neque
+                accusantium iure illum, id autem amet? Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Iure odit distinctio omnis
+                totam? Dignissimos, consectetur similique alias laborum
+                distinctio voluptas sequi sapiente veritatis neque accusantium
+                iure illum, id autem amet?
+              </p>
+            </div>
+          </div>
+          <div className="hidden lg:block w-full md:w-3/12 bg-white shadow-lg p-4 rounded-lg">
+            <ul className="">
+              {disclosure_navLink.map((item) => (
+                <li
+                  key={item.id}
+                  className="border-b-2 py-2 px-2 hover:bg-gray-100 hover:shadow-md"
+                >
+                  <Link href={item.path}>{item.display}</Link>
+                </li>
+              ))}
+            </ul>
+            <div className="flex flex-col gap-4 py-6">
+              <div className="bg-white shadow-md rounded-full flex flex-row items-center justify-start gap-4 hover:scale-105 transition duration-500">
+                <div className="bg-orange-500 p-2 w-14 h-14 rounded-full flex items-center justify-center">
+                  <SlEarphonesAlt className="w-6 h-6 text-white" />
+                </div>
+                <div className="text-base font-semibold text-neutral-700">
+                  +91 9038764817
+                </div>
+              </div>
+              <div className="bg-white shadow-md rounded-full flex flex-row items-center justify-start gap-4 hover:scale-105 transition duration-500">
+                <div className="bg-orange-500 p-2 w-14 h-14 rounded-full flex items-center justify-center">
+                  <AiOutlineMail className="w-6 h-6 text-white" />
+                </div>
+                <div className="text-base font-semibold text-neutral-700">
+                  choiceyourtrip@gmail.com
+                </div>
+              </div>
+              <div className="bg-white shadow-md rounded-full flex flex-row items-center justify-start gap-4 hover:scale-105 transition duration-500">
+                <div className="bg-orange-500 p-2 w-14 h-14 rounded-full flex items-center justify-center">
+                  <SlLocationPin className="w-6 h-6 text-white" />
+                </div>
+                <div className="text-base font-semibold text-neutral-700">
+                  Calangute Goa 403513
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+
+        <div className="p-4"></div>
+      </Container>
     </section>
   );
 };
--- frontend/src/pages/terms-conditions.tsx
+++ frontend/src/pages/terms-conditions.tsx
@@ -1,5 +1,29 @@
+import Container from "@/components/Container";
+
 const TermsConditions = () => {
-  return <div>Terms & Conditions</div>;
+  return (
+    <section className="font-poppins">
+      <Container>
+        <div className="p-4">
+          <div className="">
+            <h2 className="text-2xl font-bold">Terms & Conditions</h2>
+            <span className="text-base text-neutral-600">
+              Effective from 1st Jan 2021
+            </span>
+          </div>
+          <div className="disclosure">
+            <h4>Introduction</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure odit
+              distinctio omnis totam? Dignissimos, consectetur similique alias
+              laborum distinctio voluptas sequi sapiente veritatis neque
+              accusantium iure illum, id autem amet?
+            </p>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
 };
 
 export default TermsConditions;
