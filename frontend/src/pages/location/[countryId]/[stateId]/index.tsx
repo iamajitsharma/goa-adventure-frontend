b449df465925a6c8d0ef07c61e50a6ebc5389334
Ajit Sharma <ajitsharma2021@gmail.com>
2023-05-29 11:48:39 +0530

    Location Structure Added

---
+++ frontend/src/pages/location/[countryId]/[stateId]/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>State Name</div>;
+};
+
+export default index;
---
+++ frontend/src/pages/location/[countryId]/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>Country Name</div>;
+};
+
+export default index;
