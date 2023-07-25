445213761adb6936b70a6596960fadeca5435e6d
silverag2002 <silverag2002@gmail.com>
2023-07-25 10:42:13 +0530

    Added Wishlist

--- frontend/src/components/WishList/WishListCard.tsx
+++ frontend/src/components/WishList/WishListCard.tsx
@@ -1,6 +1,6 @@
 import Date from "../common/Date";
-import TourInfo from "./TourInfo";
-export default function BookingHistoryCard(props: any) {
+import WishInfo from "./WishInfo";
+export default function WishListCard(props: any) {
   let newDate: any = "2023-07-24T11:43:43.919Z";
   console.log("TOdya's data", newDate);
 
@@ -8,7 +8,7 @@ export default function BookingHistoryCard(props: any) {
     <>
       <div className="flex flex-row shadow-[0_3px_10px_rgb(0,0,0,0.4)] py-5 px-1 mx-3 rounded-2xl">
         <Date dateOfBooking={newDate} />
-        <TourInfo tourData={props.bookingInfo} />
+        <WishInfo tourData={props.bookingInfo} />
       </div>
     </>
   );
