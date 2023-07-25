1b73c5568aaa597d733788eb0d1dd21c4ff9138c
silverag2002 <silverag2002@gmail.com>
2023-07-25 10:18:18 +0530

    Added slider in booking-history

--- frontend/src/pages/booking-history/index.tsx
+++ frontend/src/pages/booking-history/index.tsx
@@ -1,28 +1,5 @@
 import React, { useState } from "react";
-import Container from "@/components/common/Container";
-import SearchBar from "@/components/common/SearchBar";
-import ProductList from "@/components/UI/ProductList";
-import { products } from "../../data/ActivityData";
-import { BsSearch, BsCurrencyRupee } from "react-icons/bs";
-import MultiRangeSlider from "@/components/MultiRangeSlider/MultiRangeSlider";
-import { SlArrowDown, SlArrowUp } from "react-icons/sl";
-import { IoGridOutline } from "react-icons/io5";
-import { AiOutlineBars } from "react-icons/ai";
-import Image from "next/image";
-import CoverImage from "../../../public/assets/cover.jpeg";
-import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
-import ProductCardAlt from "@/components/UI/ProductCardAlt";
-import DestinationListItems from "@/components/SearchPage/DestinationListItems";
-import HorizontalLayout from "@/components/SearchPage/HorizontalLayout";
-import VerticalLayout from "@/components/SearchPage/VerticalLayout";
-
 import BookingHistoryCard from "@/components/Book-History/BookingHistoryCard";
-
-interface ActivitySearchProps {
-  min?: any;
-  max?: any;
-}
 
 let data = [
   {
@@ -70,12 +47,13 @@ const destinationList = [
   "Rajasthan",
 ];
 
-const index: React.FC<ActivitySearchProps> = ({ min, max }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [productSwitch, setProductSwitch] = useState(false);
+const index: React.FC = () => {
   return (
     <>
-      <div className="my-4">
+      <div className="bg-white">
+        <MobileCategorySlider />
+      </div>
+      <div className="my-4 -z-10">
         {data.map((d) => (
           <BookingHistoryCard bookingInfo={d} />
         ))}
@@ -87,3 +65,70 @@ const index: React.FC<ActivitySearchProps> = ({ min, max }) => {
 };
 
 export default index;
+
+import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+const categories = [
+  { page: "/test", category: "Booking History" },
+  { page: "/test", category: "Wishlist" },
+  { page: "/test", category: "Profile Setting" },
+  { page: "/test", category: "Booking History" },
+  { page: "/test", category: "Wishlist" },
+  { page: "/test", category: "Profile Setting" },
+];
+
+const MobileCategorySlider = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    pauseOnHover: true,
+
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 2,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Slider {...settings} className="">
+        {categories.map((item, index) => (
+          <div className="text-nowrap py-2  border text-center border-black">
+            {item.category}
+          </div>
+        ))}
+      </Slider>
+    </>
+  );
+};
